7473e37e91bbf5b83a8497d422fcac470575a694
Alex Malyutin <malutin.alex@gmail.com>
2018-03-10 09:14:11 +0300

    ..

--- TaskA/TaskA.py
+++ TaskA/TaskA.py
@@ -1,8 +1,58 @@
+from __future__ import absolute_import
 import os, re, json
 import numpy as np
 import pandas as pd
 from time import time
+from munging import *
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectKBest, SelectFromModel
+
+def find_bestRF(drop='', fea='', scoring='roc_auc', title='', selectK='', fillna=True, f='FeaturesBIN3',
+                t='Targets2016', cut=False, parallel=[4, 2], required='', phone='Operator'):
+    """Testing linear method for train"""
+    train_X, train_Y = loading()
+
+    N = [20, 50, 100, 200, 500, 1000]
+    N_v2 = [500, 1000, 2000]
+    grid = [
+        {'criterion': ('gini', 'entropy'),
+         'n_estimators': N,
+         'max_features': [0.5, 'log2', 'auto', None],
+         'max_depth': [None, 10]
+         }
+    ]
+    grid_light = [
+        {'n_estimators': N_v2,
+         'max_features': [0.3, 0.4, 0.5],
+         'max_depth': [3, 5, 8, 10]
+         }
+    ]
+    # KFold for splitting
+    cv = KFold(n_splits=5,
+               shuffle=True,
+               random_state=241)
+    rf = RandomForestClassifier(random_state=241, n_jobs=-1)
+
+    test_X = loading_test()
+
+    clf = GridSearchCV(estimator=rf,
+                       param_grid=grid_light,
+                       scoring=scoring,
+                       cv=cv,
+                       n_jobs=parallel[0],
+                       pre_dispatch=parallel[1],
+                       verbose=2)
+    # ---- EPIC HERE ----
+    clf.fit(train_X, train_Y)
+    result = pd.DataFrame(columns=['id', 'proba'])
+    result['id'] = test_X['id']
+    result['proba'] = pd.DataFrame(clf.best_estimator_.fit(train_X, train_Y).predict_proba(test_X))
+    result.to_csv('result.csv')
 
 
 if __name__ == '__main__':
+    # data = pd.read_csv('../data/train_data.csv')
+    find_bestRF()
     print('Great!')
---
+++ TaskA/munging/munging.py
@@ -0,0 +1,92 @@
+import multiprocessing
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn import preprocessing
+from MultiColumnEncoder import *
+
+ewma = pd.Series.ewm
+titles = ['v_l', 'sum_b', 'percent']
+from datetime import datetime
+from tqdm import tqdm, tqdm_pandas
+
+
+# types = {'time', 'date': np.datetime, 'v_l', 'q', 'n_tr', 'sum_b', 'code_azs', 'id', 'first_prch', 'location', 'region', 'code', 'code1', 'percent', 'type'}
+
+
+def loading(path='/Users/lex/Dev/GitHub/AI.Hack/data/train_data.csv'):
+    """
+    Simple uploading data function
+    :param data: raw data in
+    :return:
+    """
+    # raw_data = pd.read_csv('ex.csv', index_col=0)
+    raw_data = pd.read_csv(path, parse_dates=['date'], index_col=0, low_memory=False)
+    raw_data['time'].fillna('04:00:00', inplace=True)
+    raw_data['month'] = raw_data['date'].apply(lambda x: x.month)
+    raw_data['year'] = raw_data['date'].apply(lambda x: x.year)
+
+    # raw_data['date'] = raw_data[['date', 'time']].apply(
+    #     lambda x: pd.to_datetime(x[0] + ' ' + x[1], errors='coerce'), axis=1)
+    groups = raw_data.groupby('id')
+    data = pd.DataFrame(columns=['v_l', 'sum_b', 'percent', 'type', 'target'])
+    for group in groups:
+        group[1].sort_values(by='date', inplace=True)
+        d = group[1].groupby(['year', 'month'], as_index=False).agg(
+            {'v_l': np.mean, 'sum_b': np.sum, 'percent': np.sum, 'type': lambda x: stats.mode(x)[0]})
+        d['target'] = d['month'].shift(-1) - d['month']
+        d['target'] = d['target'].apply(lambda x: 1 if x in [1, -11] else 0)
+        d.drop(['year', 'month'], axis=1, inplace=True)
+        scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+        d[titles] = pd.DataFrame(scaler.fit_transform(d[titles]),
+                                 index=d.index, columns=titles)
+        data = data.append(d)
+        # le = MultiColumnEncoder.EncodeCategorical(columns=['type']).fit(d)
+        # data = le.transform(d)
+        # fwd = ewma(sum, com=0.3).mean()  # take EWMA in fwd direction
+        # bwd = ewma(sum[::-1], com=0.3).mean()  # take EWMA in bwd direction
+        # filtered = np.vstack((fwd, bwd[::-1]))  # lump fwd and bwd together
+        # filtered = np.mean(filtered, axis=0)
+        # plt.plot(group[1]['date'], group[1]['sum_b'])
+        # plt.show()
+        print('t')
+    t_t = list(data)
+    t_t.remove('target')
+    X = data[t_t]
+    # Y = data[list(data_target)[1:2]]
+    Y = data['target']
+    # data_stat(X)
+    return X, Y
+
+
+def loading_test(path='/Users/lex/Dev/GitHub/AI.Hack/data/test_data.csv'):
+    """
+    Simple uploading data function
+    :param data: raw data in
+    :return:
+    """
+    # raw_data = pd.read_csv('ex.csv', index_col=0)
+    raw_data = pd.read_csv(path, parse_dates=['date'], index_col=0, low_memory=False)
+    raw_data['time'].fillna('04:00:00', inplace=True)
+    raw_data['month'] = raw_data['date'].apply(lambda x: x.month)
+    raw_data['year'] = raw_data['date'].apply(lambda x: x.year)
+
+    # raw_data['date'] = raw_data[['date', 'time']].apply(
+    #     lambda x: pd.to_datetime(x[0] + ' ' + x[1], errors='coerce'), axis=1)
+    groups = raw_data.groupby('id')
+    data = pd.DataFrame(columns=['id', 'v_l', 'sum_b', 'percent', 'type'])
+    for group in groups:
+        group[1].sort_values(by='date', inplace=True)
+        d = group[1].groupby(['year', 'month'], as_index=False).agg(
+            {'v_l': np.mean, 'sum_b': np.sum, 'percent': np.sum, 'type': lambda x: stats.mode(x)[0]})
+        d['target'] = d['month'].shift(-1) - d['month']
+        d['target'] = d['target'].apply(lambda x: 1 if x in [1, -11] else 0)
+        d.drop(['year', 'month', 'target'], axis=1, inplace=True)
+        scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+        d[titles] = pd.DataFrame(scaler.fit_transform(d[titles]),
+                                 index=d.index, columns=titles)
+        cur_row = d.tail(1)
+        cur_row['id'] = group[0]
+        data = data.append(cur_row)
+        print('t')
+    return data
